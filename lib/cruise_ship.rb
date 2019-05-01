81334466c8be921a4383e6f5cffe281074ef52e0
Joe Jamsky <cba1067950@yahoo.com>
2019-05-01 03:29:04 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,5 +8,10 @@
 
 def select_winner(passengers)
   # add the code snippet here!
-  
+  winner = ""
+  passengers.each do |key, name|
+    if passengers[:suite_a] == key && name.start_with?("A")
+      winner = name
+    end
+  end
 end
